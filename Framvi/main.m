285d55252afc30b3a63003db8bb03a6335b7d804
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-02-23 20:40:09 +0100

    First commit

---
+++ Framvi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Framvi
+//
+//  Created by Ramon Gilabert Llop on 2/23/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
